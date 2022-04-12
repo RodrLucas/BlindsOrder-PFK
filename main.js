b279c5ab02af0d3d32c5258210147c68e5543d3d
Lucas Rodrigues <lucasrodriguessantos0@gmail.com>
2022-04-12 15:43:22 -0300

    Adding more work rights

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const btn = document.getElementById("btn")
 
 
-function calculaM2(){
+function calculaM2() {
     const valorMetroQuadrado = document.getElementById("valorM2").value
     const valorInstalacao = document.getElementById("valorInst").value
-    const valorBando = document.getElementById("valorBand").value
+    const valorBando = document.getElementById("bandos").value
     const larg = document.getElementById("larg").value
     const alt = document.getElementById("alt").value
     const porcentagem = document.getElementById("porcentagem").value
@@ -12,27 +12,56 @@ function calculaM2(){
 
     let m2 = larg * alt;
 
-    if(m2 < 1.5){
-        m2 = 1.5
-        //Precisei declarar o tipo da variável para ter certeza que era tipo number 
-        const result = Number(m2) * Number(valorMetroQuadrado) + Number(valorInstalacao) + Number(valorBando)
-        if(!porcentagem > 0) {
-            return resultado.innerHTML = Intl.NumberFormat('pt-BR',{ style: 'currency', currency: 'BRL' }).format(result);
-        }else {
-            const percentValue = (result / 100) * porcentagem
-            const finalValueWithPercent = result + percentValue
-            resultado.innerHTML = Intl.NumberFormat('pt-BR',{ style: 'currency', currency: 'BRL' }).format(finalValueWithPercent);
+    if (valorBando === "branco") {
+        let valor = 177.10 * larg
+        if (m2 < 1.5) {
+            m2 = 1.5
+            //Precisei declarar o tipo da variável para ter certeza que era tipo number 
+            const result = Number(m2) * Number(valorMetroQuadrado) + Number(valorInstalacao) + Number(valor)
+            if (!porcentagem > 0) {
+                return resultado.innerHTML = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(result);
+            } else {
+                const percentValue = (result / 100) * porcentagem
+                const finalValueWithPercent = result + percentValue
+                resultado.innerHTML = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(finalValueWithPercent);
+            }
+
+        } else {
+            const result = Number(m2) * Number(valorMetroQuadrado) + Number(valorInstalacao) + Number(valor)
+            if (!porcentagem > 0) {
+                return resultado.innerHTML = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(result);
+            } else {
+                const percentValue = (result / 100) * porcentagem
+                const finalValueWithPercent = result + percentValue
+                resultado.innerHTML = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(finalValueWithPercent);
+            }
         }
-    }else{
-        const result = Number(m2) * Number(valorMetroQuadrado) + Number(valorInstalacao) + Number(valorBando)
-        if(!porcentagem > 0) {
-            return resultado.innerHTML = Intl.NumberFormat('pt-BR',{ style: 'currency', currency: 'BRL' }).format(result);
-        }else {
-            const percentValue = (result / 100) * porcentagem
-            const finalValueWithPercent = result + percentValue
-            resultado.innerHTML = Intl.NumberFormat('pt-BR',{ style: 'currency', currency: 'BRL' }).format(finalValueWithPercent);
+
+    }else {
+        let valor = 189.75 * larg
+        if (m2 < 1.5) {
+            m2 = 1.5
+            //Precisei declarar o tipo da variável para ter certeza que era tipo number 
+            const result = Number(m2) * Number(valorMetroQuadrado) + Number(valorInstalacao) + Number(valor)
+            if (!porcentagem > 0) {
+                return resultado.innerHTML = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(result);
+            } else {
+                const percentValue = (result / 100) * porcentagem
+                const finalValueWithPercent = result + percentValue
+                resultado.innerHTML = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(finalValueWithPercent);
+            }
+        } else {
+            const result = Number(m2) * Number(valorMetroQuadrado) + Number(valorInstalacao) + Number(valor)
+            if (!porcentagem > 0) {
+                return resultado.innerHTML = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(result);
+            } else {
+                const percentValue = (result / 100) * porcentagem
+                const finalValueWithPercent = result + percentValue
+                resultado.innerHTML = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(finalValueWithPercent);
+            }
         }
     }
+
 }
 
 
